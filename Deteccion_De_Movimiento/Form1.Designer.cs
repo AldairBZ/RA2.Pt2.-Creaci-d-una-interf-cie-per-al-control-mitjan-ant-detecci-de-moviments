8a8a7b18033f90f0c4ec26a7ba983d475743940d
mana819 <aldairbz1719@gmail.com>
2026-03-09 18:44:58 +0100

    v.1.0.0
    new

---
+++ Deteccion_De_Movimiento/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Deteccion_Movimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbVideo = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbVideo
+            // 
+            this.pbVideo.Location = new System.Drawing.Point(184, 38);
+            this.pbVideo.Name = "pbVideo";
+            this.pbVideo.Size = new System.Drawing.Size(376, 237);
+            this.pbVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbVideo.TabIndex = 0;
+            this.pbVideo.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(244, 310);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(114, 43);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Comenzar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(382, 310);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 43);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Pausar";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbVideo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Deteccion_De_Movimiento/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum; // <--- Asegúrate de que esta línea esté presente
+namespace Deteccion_Movimiento
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture; // Captura de la cámara
+        private Mat _frameAnterior = new Mat(); // Para comparar frames
+        private bool _isCapturing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¡Botón pulsado!");
+            if (_capture == null)
+            {
+                _capture = new VideoCapture(0, VideoCapture.API.DShow);
+                // El evento ImageGrabbed se activa cada vez que la cámara tiene un frame nuevo
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            _capture.Start();
+            _isCapturing = true;
+
+            if (_capture.IsOpened)
+            {
+                MessageBox.Show("La cámara se abrió correctamente");
+            }
+            else
+            {
+                MessageBox.Show("No se pudo abrir la cámara. Revisa la conexión en la VM.");
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_isCapturing && _capture != null)
+            {
+                Mat frameActual = new Mat();
+
+                // "Retrieve" saca la foto de la cámara y la mete en frameActual
+                _capture.Retrieve(frameActual);
+
+                if (!frameActual.IsEmpty)
+                {
+                    // LÓGICA PARA LA DETECCIÓN DE MOVIMIENTO
+                    if (!_frameAnterior.IsEmpty)
+                    {
+                        Mat diff = new Mat();
+                        // Comparamos el frame actual con el anterior
+                        CvInvoke.AbsDiff(frameActual, _frameAnterior, diff);
+                        CvInvoke.CvtColor(diff, diff, ColorConversion.Bgr2Gray);
+                        CvInvoke.Threshold(diff, diff, 25, 255, ThresholdType.Binary);
+
+                        // Contamos píxeles que han cambiado
+                        int m = CvInvoke.CountNonZero(diff);
+
+                        // Si hay movimiento (más de 500 píxeles), ponemos el fondo en rojo
+                        this.Invoke(new Action(() => {
+                            if (m > 3000)
+                            {
+                                this.BackColor = Color.Red;
+                            }
+                            else
+                            {
+                                this.BackColor = SystemColors.Control;
+                            }
+                        }));
+                    }
+
+                    // Guardamos el frame actual para compararlo en la siguiente vuelta
+                    frameActual.CopyTo(_frameAnterior);
+
+                    // DIBUJAMOS LA IMAGEN EN EL PICTUREBOX
+                    pbVideo.Invoke(new Action(() => {
+                        // Usamos la extensión para convertir Mat a algo que el PictureBox entienda
+                        pbVideo.Image = Emgu.CV.BitmapExtension.ToBitmap(frameActual);
+                    }));
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.Stop();
+                _isCapturing = false;
+            }
+        }
+    }
+}
